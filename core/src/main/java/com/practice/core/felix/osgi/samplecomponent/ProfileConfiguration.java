a6a1ec123b4ad3d3edae34b44c3f7f06ff43be93
Prakash Reddy <prreddy@merkleinc.com>
2018-05-21 23:55:24 +0530

    OSGI configuration run mode specific changes

--- core/src/main/java/com/practice/core/felix/osgi/samplecomponent/ProfileConfiguration.java
+++ core/src/main/java/com/practice/core/felix/osgi/samplecomponent/ProfileConfiguration.java
@@ -2,36 +2,39 @@ package com.practice.core.felix.osgi.samplecomponent;
 
 import java.util.Map;
 
+import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.PropertyOption;
+import org.apache.sling.commons.osgi.PropertiesUtil;
 
 @Component(name = "Profile Configuration", immediate = true, metatype = true)
 public class ProfileConfiguration {
 
-	@Property(name = "User Name", value = "", description = "Profile username", label = "Enter User Name")
+	@Property(value = "", description = "Profile username", label = "Enter User Name")
 	private static final String USER_NAME = "user.name";
 	private String username;
 
-	@Property(name = "Password", description = "Profile username", label = "Enter User Name", passwordValue = "")
+	@Property(description = "Profile username", label = "Enter User Name", passwordValue = "")
 	private static final String PASSWORD = "user.password";
 	private String password;
 
-	@Property(name = "Gender", description = "Gender", label = "Gender", options = {
-			@PropertyOption(name = "Male", value = "male"),
-			@PropertyOption(name = "Female", value = "female") }, value = "female")
+	@Property(description = "Gender", label = "Gender", options = {
+			@PropertyOption(name = "male", value = "Male"),
+			@PropertyOption(name = "female", value = "Female") }, value = "female")
 	private static final String GENDER = "user.gender";
 	private String gender;
 	
-	@Property(name ="Admin", description="Is Admin User", label="Is Admin", boolValue=false)
+	@Property(description="Is Admin User", label="Is Admin", boolValue=false)
 	private static final String IS_ADMIN  = "is.admin";
 	private Boolean isAdmin;
 	
+	@Activate
 	protected void activate(Map<String, Object> props){
-		this.username = (String) props.get(USER_NAME);
-		this.password  = (String) props.get(PASSWORD);
-		this.gender = (String) props.get(GENDER);
-		this.isAdmin = (Boolean) props.get(IS_ADMIN);
+		this.username = PropertiesUtil.toString(props.get(USER_NAME), "");
+		this.password  = PropertiesUtil.toString(props.get(PASSWORD), "");
+		this.gender = PropertiesUtil.toString(props.get(GENDER), "");
+		this.isAdmin = PropertiesUtil.toBoolean(IS_ADMIN, false);
 	}
 
 }
